28f2ed99bd2bed10cbdf8d4fafe9c523bd90fe70
JaredMartin0351 <jaredmartin0351@gmail.com>
2021-11-23 11:51:38 -0600

    push to change hands
    ran test to get quarter, nickel, dime and penny, test successful

---
+++ test_customer.py
@@ -0,0 +1,60 @@
+import unittest
+import user_interface
+from wallet import Wallet
+from customer import Customer
+from backpack import Backpack
+
+
+class TestGetWalletCoin(unittest.TestCase):
+    def setUp(self):
+        self.customer = Customer()
+    
+    
+    def test_get_quarter(self):
+        """Passes in the 'Quarter' object, should return the quarter object"""
+        returned_coin = self.customer.get_wallet_coin('Quarter')
+        self.assertEqual(returned_coin.name, 'Quarter')
+        
+    def test_get_dime(self):
+        """Passes in the 'Dime' object, should return the dime object"""
+        returned_coin = self.customer.get_wallet_coin('Dime')
+        self.assertEqual(returned_coin.name, 'Dime')
+        
+    def test_get_nickel(self):
+        """Passes in the 'Nickel' object, should return the nickel object"""
+        returned_coin = self.customer.get_wallet_coin('Nickel')
+        self.assertEqual(returned_coin.name, 'Nickel')
+        
+    def test_get_penny(self):
+        """Passes in the 'Penny' object, should return the penny object"""
+        returned_coin = self.customer.get_wallet_coin('Penny')
+        self.assertEqual(returned_coin.name, 'Penny')
+        
+
+    
+    
+class TestAddCoinsToWallet(unittest.TestCase):
+    def setUp(self):
+        self.customer = Customer()
+        
+        
+    def test_add_quarter(self):
+        added_coin = self.customer.add_coins_to_wallet()
+        self.assertEqual(added_coin.name, )
+        
+#    def test_add_dime(self):
+#        added_coin = self.customer.add_coins_to_wallet('Dime')
+#        self.assertEqual(added_coin.name, 'Dime')
+        
+#    def test_add_nickel(self):
+#        added_coin = self.customer.add_coins_to_wallet('Nickel')
+#        self.assertEqual(added_coin.name, 'Nickel')
+        
+#    def test_add_penny(self):
+#        added_coin = self.customer.add_coins_to_wallet('Penny')
+#        self.assertEqual(added_coin.name, 'Penny')
+        
+if __name__ == '__main__':
+    unittest.main()
+    
+    
---
+++ test_wallet.py
@@ -0,0 +1,5 @@
+import unittest
+from wallet import Wallet
+import coins
+
+class Test
