94901322bf96c353977ad41d9d0df7e8f900b1f2
unknown <dallas.runion10@gmail.com>
2021-11-23 16:36:24 +0300

    finished all of the testing classes with methods for test_user_interface file

--- test_user_interface.py
+++ test_user_interface.py
@@ -38,10 +38,12 @@ class Testuserinterface(unittest.TestCase):
 
 class TestTryParse(unittest.TestCase):
     def test_try_parse_int_withint(self):
+        """Testing the try_parse_int method with the in of 10"""
         truenumber = user_interface.try_parse_int(10)
         self.assertEqual(truenumber,10)
 
     def test_try_parse_int_withstr(self):
+        """Testing the try_parse_int method with a str of hello  """
         string = user_interface.try_parse_int("hello")
         self.assertEqual(string,0)
 
@@ -60,6 +62,7 @@ class TestTryParse(unittest.TestCase):
 # b. Pass in “hello”, ensure 0 is returned
 class TestGetUniqueCanNames(unittest.TestCase):
     def test_can_name_list(self):
+        """Testing the get_unique_can_names method with multiple of the same cans """
         rootbeer = RootBeer()
         rootbeer2 = RootBeer()
         cola = Cola()
@@ -77,6 +80,7 @@ class TestGetUniqueCanNames(unittest.TestCase):
         self.assertNotEqual(can_checker,list_of_can)
 
     def test_can_list_return(self):
+        """Testing the get_unique_can_names method with a blank list to see if return is still the same"""
         can_list = []
         check_list = user_interface.get_unique_can_names(can_list)
 
@@ -104,6 +108,7 @@ class TestGetUniqueCanNames(unittest.TestCase):
 
 class TestDisplayPaymentValue(unittest.TestCase):
     def test_coin_value(self):
+        """Testing the display_payment_value method with all coins that add up to .41"""
         quarter = Quarter()
         dime = Dime()
         nickel = Nickel()
@@ -115,11 +120,44 @@ class TestDisplayPaymentValue(unittest.TestCase):
         list_value_41.append(penny)
         truelist = user_interface.display_payment_value(list_value_41)
         self.assertEqual(truelist , .41)
+    def test_empty_list(self):
+        """Testing the display_payment_value method with a blank list to get a return value of 0"""
+        list_value_0 = []
+        list0 = user_interface.display_payment_value(list_value_0)
+        self.assertEqual(list0,0)
+
 # display_payment_value – 2 tests
 # a. Instantiate each of the 4 coin types and append them to a list. Pass the list into this function, ensure the returned values is .41
 # b. Pass in an empty list. Ensure the returned value is 0.
 
+class TestValidateCoinSelection(unittest.TestCase):
+    
+    def test_coin_sel_1(self):
+        """Testing validate_coin_selection with input of (1) to get True and Quarter as a return"""
+        quarter_tuple = user_interface.validate_coin_selection(1)
+        self.assertEqual(quarter_tuple,(True, "Quarter"))
+    
+    def test_coin_sel_2(self):
+        """Testing validate_coin_selection with input of (2) to get True and Dime as a return"""
+        dime_tuple = user_interface.validate_coin_selection(2)
+        self.assertEqual(dime_tuple,(True, "Dime"))
+    
+    def test_coin_sel_3(self):
+        """Testing validate_coin_selection with input of (3) to get True and Nickel as a return"""
+        nickel_tuple = user_interface.validate_coin_selection(3)
+        self.assertEqual(nickel_tuple,(True, "Nickel"))
+    
+    def test_coin_sel_4(self):
+        """Testing validate_coin_selection with input of (4) to get True and Penny as a return"""
+        penny_tuple = user_interface.validate_coin_selection(4)
+        self.assertEqual(penny_tuple,(True, "Penny"))
 
+    def test_coin_sel_other(self):
+        """Testing validate_coin_selection with input other than 1,2,3,4
+         to get False and None as a return"""
+        otherinput = range(random.randint(5,150))
+        other_tuple = user_interface.validate_coin_selection(otherinput)
+        self.assertEqual(other_tuple,(False, None))
 
 
 if __name__ == "__main__":
