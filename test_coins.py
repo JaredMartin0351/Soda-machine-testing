a96fbb0bb366ec7dae888beb1d6a4ae8bbf408f9
JaredMartin0351 <jaredmartin0351@gmail.com>
2021-11-23 13:56:37 -0600

    created test coins to get coin name
    created test coins functions to test the coin names, worked successfully

--- test_coins.py
+++ test_coins.py
@@ -1,10 +1,25 @@
-from coins import Quarter
+from coins import Dime, Quarter, Nickel, Penny
 import unittest
 
 
-class Test_QuarterName(unittest.TestCase):
+class TestCoinName(unittest.TestCase):
     
     def test_quarter_name(self):        
         quarter = Quarter()
         self.assertEqual('Quarter', quarter.name)
+        
+    def test_dime_name(self):        
+        dime = Dime()
+        self.assertEqual('Dime', dime.name)
+        
+    def test_nickel_name(self):        
+        nickel = Nickel()
+        self.assertEqual('Nickel', nickel.name)
+        
+    def test_penny_name(self):        
+        penny = Penny()
+        self.assertEqual('Penny', penny.name)
+        
+if __name__ == '__main__':
+    unittest.main()
         
--- test_wallet.py
+++ test_wallet.py
@@ -2,4 +2,7 @@ import unittest
 from wallet import Wallet
 import coins
 
-class Test
+class TestFillWallet(unittest.TestCase):
+    def setUp(self):
+        self.wallet = Wallet()
+        self.money = []
